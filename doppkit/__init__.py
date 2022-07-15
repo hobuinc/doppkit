3451e697035b29abb138356453235aff3ed46288
Ognyan Moore <ognyan.moore@gmail.com>
2022-07-15 15:32:11 -0700

    Restructure project layout per pytest recommendations

--- doppkit/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "0.0.5"
-
-from .app import Application
--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-import doppkit
-NAME = "doppkit"
-VERSION = doppkit.__version__
-
-REQUIRES = [
-    "aiohttp",
-    "werkzeug",
-    "httpx",
-    "rich",
-    "click"
-]
-
-setup(
-    name=NAME,
-    version=VERSION,
-    description="GRiD API v3",
-    author_email="",
-    url="https://github.com/hobuinc/doppkit",
-    install_requires=REQUIRES,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    entry_points={
-        'console_scripts': ['doppkit=doppkit.__main__:main'],
-    },
-    long_description="""\
-    GRiD Synchronization client
-    """
-)
+setup()
